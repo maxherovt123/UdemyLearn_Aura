c0eab5244f2b0c7cb30da54d9a011ab2bad97e83
Trung Nguyen <maxherovt123@gmail.com>
2024-10-08 21:54:04 -0400

    Health & Mana

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -5,7 +5,6 @@
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "AuraAttributeSet.generated.h"
-
 /**
  * 
  */
@@ -13,5 +12,32 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(Blueprintable, BlueprintReadOnly,ReplicatedUsing = OnRep_Health, Category="Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(Blueprintable,BlueprintReadOnly,ReplicatedUsing = OnRep_MaxHealth, Category="Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(Blueprintable, BlueprintReadOnly,ReplicatedUsing = OnRep_Mana, Category="Vital Attributes")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(Blueprintable,BlueprintReadOnly,ReplicatedUsing = OnRep_MaxMana, Category="Vital Attributes")
+	FGameplayAttributeData MaxMana;
 	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
+
